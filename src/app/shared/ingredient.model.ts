64c4fc2cab70be14cfd710c7c34e70ff27a90268
rahulchanglani <rahul.changlani@gmail.com>
2017-07-11 14:06:11 +0530

    proj part 1

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient {
+
+  constructor(public name: string, public amount: number) {
+    // Typescript fature - To assign class access type in args of constructor to assign and initialize local variables (properties) to constructor params
+  }
+}
